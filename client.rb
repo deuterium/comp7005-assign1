c6108c5fcbc99c01da970b7d53e6ba8dce9108ba
Chris Wood <chriswood.ca@gmail.com>
2013-09-17 19:34:27 -0700

    First commit
    Empty readme
    basic skeleton client code

---
+++ client.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+#Chris Wood - A00741285 - COMP7005 - Assignment 1
+#descriptionnnn
+
+##### CLIENT CODE
+default_port = 7005
+
+
+
+require 'socket'
+
+
+
+
+
+#check invocation and proper usage
+
+#connect
+
+#send
+
+#get
+
+#disconnect
+
+
+#always error check for api calls (socket creation, socket binding, etc)
+
+
